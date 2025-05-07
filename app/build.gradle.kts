109f1ffe097eeac6007878b479e6a5bd6cf0e520
MezoPeeta <wildlifemain1@gmail.com>
2025-05-07 23:29:49 +0300

    Stabelize mostaqem and get it ready for google play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.mostaqem"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
