2b3b0d0d60d276f659f8a730cbbdd98e514fac13
MezoPeeta <wildlifemain1@gmail.com>
2025-05-13 01:44:41 +0300

    added share quran

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "1.0.0"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
